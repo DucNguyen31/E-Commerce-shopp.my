c97884aa3062780238af401b992dcafcef3aafa6
Nguyen Sy Duc <94820113+DucNguyen31@users.noreply.github.com>
2022-04-12 23:32:13 +0700

    Update DBContext.java

--- src/java/context/DBContext.java
+++ src/java/context/DBContext.java
@@ -24,7 +24,7 @@ public class DBContext {
 
     /*Insert your other code right after this comment*/
  /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
-    private final String serverName = "localhost";
+    private final String serverName = "ADMIN";
     private final String dbName = "shop";
     private final String portNumber = "1433";
     private final String userID = "sa";
